ddb20d88cd4060c0fd2084ee452aefd23408cfd2
Felix-wainaina <nduatifelixwain@gmail.com>
2026-07-17 08:47:55 +0300

    feat: install react-router-dom and implement client-side routing between landing and dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import MyUnits from './pages/MyUnits';
 
 function App() {
   return (
-    // We just render the Landing page here for now!
-    <Landing />
-    // <MyUnits />
-  )
+    <Router>
+      <Routes>
+        {/* The home page (Landing) */}
+        <Route path="/" element={<Landing />} />
+        
+        {/* The dashboard page (My Units) */}
+        <Route path="/my-units" element={<MyUnits />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
